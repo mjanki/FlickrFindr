7df3fbf5c4064b9224dd1e99baba30ee05c23e8e
Mou Janki <mjanki1990@gmail.com>
2020-02-17 16:40:06 -0500

    Kept Error tests

--- view/src/androidTest/java/org/br/flickrfinder/MainFragmentTest.kt
+++ view/src/androidTest/java/org/br/flickrfinder/MainFragmentTest.kt
@@ -1,16 +1,15 @@
 package org.br.flickrfinder
 
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import org.br.flickrfinder.activities.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.br.flickrfinder.activities.MainActivity
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -21,16 +20,7 @@ class MainFragmentTest {
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun findFabAddTask_shouldExist() {
-        Espresso.onView(ViewMatchers.withId(R.id.fabAddTask)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-    }
-
-    @Test
-    fun clickFabAddTask_threeTimes_shouldDisplayThreeTasks() {
-        Espresso.onView(ViewMatchers.withId(R.id.fabAddTask)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fabAddTask)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fabAddTask)).perform(ViewActions.click())
-
-        Espresso.onView(ViewMatchers.withId(R.id.rvTasks)).check(ViewAssertions.matches(ViewMatchers.hasDescendant(ViewMatchers.withText("New Task 2"))))
+    fun findButtonSearch_shouldExist() {
+        Espresso.onView(ViewMatchers.withId(R.id.bSearch)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
